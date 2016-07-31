186c03cd345585514d40c550dc25c813ffcddb9c
shainaveloz <shaina.veloz@gmail.com>
2016-07-30 22:19:42 -0400

    added a handlebars file

--- server.js
+++ server.js
@@ -2,10 +2,19 @@
 //initializing express app
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 //requiring request and cheerio
 var request = require('request');
 var cheerio = require('cheerio');
+
+//configuring app for use with body-parser
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+
+// static file support with public folder
+app.use(express.static('public'));
 
 //database configuration
 var mongojs = require('mongojs');
@@ -14,12 +23,23 @@ var collections = ["scrapedData"];
 
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections);
+
+//makes sure that any errors encountered is logged
 db.on('error', function(err) {
   console.log('Database Error:', err);
 });
 
 //Main route
 app.get('/', function(req, res){
-    res.send('Hi');
+    res.send('Hello! Welcome to Tech Insider!');
+});
+
+app.get('/scraper', function(req, res){
+
+});
+
+// listen on port 3000
+app.listen(3000, function() {
+  console.log('App running on port 3000!');
 });
 
